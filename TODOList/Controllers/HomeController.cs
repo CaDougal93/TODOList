ec43b1c8bde4a4b308f3853f9f39e6ded02a5a07
Cade McDougal <cadougal93@gmail.com>
2019-02-27 23:59:19 -0600

    Removed janky json from viewmodel that was for testing. Added some styling. Some styling still needs to be done

--- TODOList/Controllers/HomeController.cs
+++ TODOList/Controllers/HomeController.cs
@@ -16,20 +16,10 @@ namespace TODOList.Controllers
 
             using (var context = new TODOItemContext())
             {
-                var jsonSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
                 vm.TODOList = context.TODOItems.ToList();
-                vm.TODOJSON = jsonSerializer.Serialize(vm.TODOList);
             }
 
                 return View(vm);
-        }
-
-        public TODOItem Get(int id)
-        {
-            using (var context = new TODOItemContext())
-            {
-                return context.TODOItems.ToList().Where(x => x.TODOItemID == id).FirstOrDefault();
-            }
         }
 
         public void Delete(int id)
@@ -55,27 +45,30 @@ namespace TODOList.Controllers
         {
             using (var context = new TODOItemContext())
             {
-                context.TODOItems.Where(x => x.TODOItemID == id && x.IsComplete != isComplete).FirstOrDefault().IsComplete = isComplete;
+                context.TODOItems.Where(x => x.TODOItemID == id).FirstOrDefault().IsComplete = isComplete;
                 context.SaveChanges();
             }
         }
 
-        public void Add(string todoItemName)
+        public string Add(string todoItemName)
         {
+            TODOItem newItem = new TODOItem
+            {
+                TODOItemName = todoItemName,
+                IsComplete = false
+            };
+
             using (var context = new TODOItemContext())
             {
                 if(!context.TODOItems.Where(x => x.TODOItemName == todoItemName).Any() && !string.IsNullOrEmpty(todoItemName))
                 {
-                    TODOItem newItem = new TODOItem
-                    {
-                        TODOItemName = todoItemName,
-                        IsComplete = false
-                    };
-
-                    context.TODOItems.Add(newItem);
+                    newItem = context.TODOItems.Add(newItem);
                     context.SaveChanges();
+
+                    return Newtonsoft.Json.JsonConvert.SerializeObject(newItem);
                 }
             }
+            return string.Empty;
         }
     }
 }
--- TODOList/Models/ViewModels/TODOViewModel.cs
+++ TODOList/Models/ViewModels/TODOViewModel.cs
@@ -5,6 +5,5 @@ namespace TODOList.Models.ViewModels
     public class TODOViewModel
     {
         public List<TODOItem> TODOList { get; set; }
-        public string TODOJSON { get; set; }
     }
 }
